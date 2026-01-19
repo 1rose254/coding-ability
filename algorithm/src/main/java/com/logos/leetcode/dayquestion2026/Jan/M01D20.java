a7d56c3b00961ebad5bf27b70a69b50e2c037d7d
logos <2378884181@qq.com>
2026-01-20 00:28:53 +0800

    feat(algorithm): 添加最小位运算数组I题目解法
    - 新增M01D20类及内部Solution类
    - 实现minBitwiseArray方法，处理输入整数列表
    - 根据二进制位操作生成结果数组
    - 返回满足题目要求的最小位运算数组

---
+++ algorithm/src/main/java/com/logos/leetcode/dayquestion2026/Jan/M01D20.java
@@ -0,0 +1,36 @@
+package com.logos.leetcode.dayquestion2026.Jan;
+
+import java.util.List;
+
+/**
+ * @author logos
+ * date 2026/1/20 0:27
+ * @version v1.0
+ * @Package com.logos.leetcode.dayquestion2026.Jan
+ */
+public class M01D20 {
+
+    /**
+     * 3314. 构造最小位运算数组 I
+     * <a href="https://leetcode.cn/problems/construct-the-minimum-bitwise-array-i/description/"/a>
+     */
+
+    class Solution {
+        public int[] minBitwiseArray(List<Integer> nums) {
+            int n = nums.size();
+            int[] ans = new int[n];
+            for (int i = 0; i < n; i++) {
+                int x = nums.get(i);
+                if (x == 2) {
+                    ans[i] = -1;
+                } else {
+                    int t = ~x;
+                    int lowbit = t & -t;
+                    ans[i] = x ^ (lowbit >> 1);
+                }
+            }
+            return ans;
+        }
+    }
+
+}
