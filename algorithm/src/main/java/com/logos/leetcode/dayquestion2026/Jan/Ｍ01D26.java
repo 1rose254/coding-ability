b3dd28b019620840bd3b34aa5c5a97bb86e23768
logos <2378884181@qq.com>
2026-01-26 00:15:33 +0800

    feat(algorithm): 添加1200题最小绝对差的算法实现
    - 新增Jan/Ｍ01D26类和内部Solution类
    - 实现minimumAbsDifference方法计算数组中最小绝对差对
    - 使用排序和单遍扫描确定最小差值和对应数对
    - 返回所有满足最小绝对差的数对列表

---
+++ algorithm/src/main/java/com/logos/leetcode/dayquestion2026/Jan/Ｍ01D26.java
@@ -0,0 +1,43 @@
+package com.logos.leetcode.dayquestion2026.Jan;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author logos
+ * date 2026/1/26 0:14
+ * @version v1.0
+ * @Package com.logos.leetcode.dayquestion2026.Jan
+ */
+public class Ｍ01D26 {
+
+    /**
+     * 1200. 最小绝对差
+     * <a href="https://leetcode.cn/problems/minimum-absolute-difference/description/"/>
+     */
+
+    class Solution {
+        public List<List<Integer>> minimumAbsDifference(int[] arr) {
+            Arrays.sort(arr);
+            List<List<Integer>> ans = new ArrayList<>();
+            int n = arr.length;
+            int min = arr[1] - arr[0];
+            for (int i = 0; i < n - 1; i++) {
+                int cur = arr[i + 1] - arr[i];
+                if (cur < min) {
+                    ans.clear();
+                    min = cur;
+                }
+                if (cur == min) {
+                    List<Integer> temp = new ArrayList<>();
+                    temp.add(arr[i]);
+                    temp.add(arr[i + 1]);
+                    ans.add(temp);
+                }
+            }
+            return ans;
+        }
+    }
+
+}
