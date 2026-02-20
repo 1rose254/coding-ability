042211e2dcc79e64fdcec7109c7f70dd8df86ff2
logos <2378884181@qq.com>
2026-02-21 00:02:52 +0800

    feat(algorithm): 添加题目762二进制中置位为质数的统计
    - 创建M02D21类并实现Solution内部类
    - 通过哈希表预置所有质数位置
    - 实现countPrimeSetBits方法，计算区间内置位为质数的数字个数
    - 利用位运算实现高效计数置位操作
    - 添加题目描述和链接注释

---
+++ algorithm/src/main/java/com/logos/leetcode/dayquestion2026/Feb/M02D21.java
@@ -0,0 +1,41 @@
+package com.logos.leetcode.dayquestion2026.Feb;
+
+/**
+ * @author logos
+ * date 2026/2/21 0:01
+ * @version v1.0
+ * @Package com.logos.leetcode.dayquestion2026.Feb
+ */
+public class M02D21 {
+
+    /**
+     * 762. 二进制表示中质数个计算置位
+     * <a href="https://leetcode.cn/problems/prime-number-of-set-bits-in-binary-representation/description/"/>
+     */
+
+    class Solution {
+        static boolean[] hash = new boolean[40];
+
+        static {
+            int[] nums = new int[]{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31};
+            for (int x : nums) {
+                hash[x] = true;
+            }
+        }
+
+        public int countPrimeSetBits(int left, int right) {
+            int ans = 0;
+            for (int i = left; i <= right; i++) {
+                int x = i, cnt = 0;
+                while (x != 0 && ++cnt >= 0) {
+                    x -= (x & -x);
+                }
+                if (hash[cnt]) {
+                    ans++;
+                }
+            }
+            return ans;
+        }
+    }
+
+}
