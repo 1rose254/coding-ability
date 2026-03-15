f8d7385e9f66cae1bbeda16c7a4b87a25e12e7bd
logos <2378884181@qq.com>
2026-03-15 16:55:50 +0800

    feat(dp): 添加基于动态规划的最长递增子序列解法
    - 新增 Solution2 类实现最长递增子序列计算
    - 使用双层循环计算每个位置的最大子序列长度
    - 利用 dp 数组存储以每个元素结尾的最长子序列长度
    - 更新全局最大值 ans 以获取最终结果
    - 采用自增策略简化 dp[i] 赋值逻辑

--- algorithm/src/main/java/com/logos/leetcode/hot100/dp/Q07.java
+++ algorithm/src/main/java/com/logos/leetcode/hot100/dp/Q07.java
@@ -38,4 +38,21 @@ public class Q07 {
         }
     }
 
+    class Solution2 {
+        public int lengthOfLIS(int[] nums) {
+            int n = nums.length;
+            int ans = 0;
+            int[] dp = new int[n];
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < i; j++) {
+                    if (nums[j] < nums[i]) {
+                        dp[i] = Math.max(dp[i], dp[j]);
+                    }
+                }
+                ans = Math.max(ans, ++dp[i]);
+            }
+            return ans;
+        }
+    }
+
 }
