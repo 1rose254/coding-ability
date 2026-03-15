e5228e89a9635fa6622e05da206d3f6fb71b8c00
logos <2378884181@qq.com>
2026-03-15 16:53:32 +0800

    feat(dp): 添加最长递增子序列算法实现
    - 实现了基于深度优先搜索和动态规划的最长递增子序列求解
    - 使用递归和备忘录数组避免重复计算
    - 计算并更新迄今为止的最长子序列长度
    - 代码结构清晰，便于后续扩展和维护
    - 添加了类注释说明作者和版本信息

---
+++ algorithm/src/main/java/com/logos/leetcode/hot100/dp/Q07.java
@@ -0,0 +1,41 @@
+package com.logos.leetcode.hot100.dp;
+
+/**
+ * @author logos
+ * date 2026/3/15 16:46
+ * @version v1.0
+ * @Package com.logos.leetcode.hot100.dp
+ */
+public class Q07 {
+
+    /**
+     * 300. 最长递增子序列
+     */
+
+    class Solution {
+        public int lengthOfLIS(int[] nums) {
+            int n = nums.length;
+            int[] dp = new int[n];
+            int ans = 0;
+            for (int i = 0; i < n; i++) {
+                ans = Math.max(ans, dfs(i, nums, dp));
+            }
+            return ans;
+        }
+
+        private int dfs(int i, int[] nums, int[] dp) {
+            if (dp[i] > 0) {
+                return dp[i];
+            }
+            int ans = 0;
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    ans = Math.max(ans, dfs(j, nums, dp));
+                }
+            }
+            ans++;
+            return dp[i] = ans;
+        }
+    }
+
+}
