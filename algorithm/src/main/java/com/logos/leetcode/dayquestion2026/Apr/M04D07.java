788f91133fe4045c00a6b83a4d414915668a8f20
logos <2378884181@qq.com>
2026-04-07 14:15:19 +0800

    feat(algorithm): 添加模拟行走机器人 II 题解
    - 新增类 Robot 实现机器人行走模拟功能
    - 支持通过 step 方法移动机器人指定步数
    - 实现 getPos 获取机器人当前坐标
    - 实现 getDir 获取机器人当前朝向
    - 通过状态计算方法 getState 计算当前位置和方向
    - 代码基于 LeetCode 题目 2069 模拟行走机器人 II

---
+++ algorithm/src/main/java/com/logos/leetcode/dayquestion2026/Apr/M04D07.java
@@ -0,0 +1,52 @@
+package com.logos.leetcode.dayquestion2026.Apr;
+
+/**
+ * @author logos
+ * date 2026/4/7 14:13
+ * @version v1.0
+ * @Package com.logos.leetcode.dayquestion2026.Apr
+ */
+public class M04D07 {
+
+    /**
+     * 2069. 模拟行走机器人 II
+     * <a href="https://leetcode.cn/problems/walking-robot-simulation-ii/description/"/>
+     */
+
+    class Robot {
+        private int w, h, s;
+
+        public Robot(int width, int height) {
+            w = width;
+            h = height;
+            s = 0;
+        }
+
+        public void step(int num) {
+            s = (s + num - 1) % ((w + h - 2) * 2) + 1;
+        }
+
+        public int[] getPos() {
+            Object[] t = getState();
+            return new int[]{(int) t[0], (int) t[1]};
+        }
+
+        public String getDir() {
+            Object[] t = getState();
+            return (String) t[2];
+        }
+
+        private Object[] getState() {
+            if (s < w) {
+                return new Object[]{s, 0, "East"};
+            } else if (s < w + h - 1) {
+                return new Object[]{w - 1, s - w + 1, "North"};
+            } else if (s < w * 2 + h - 2) {
+                return new Object[]{w * 2 + h - s - 3, h - 1, "West"};
+            } else {
+                return new Object[]{0, (w + h) * 2 - s - 4, "South"};
+            }
+        }
+    }
+
+}
