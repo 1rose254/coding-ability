b8fbfde4b40d7a4b5c126705b8df5123bbf9e45c
logos <2378884181@qq.com>
2026-06-02 21:32:15 +0800

    feat(algorithm): 添加最早完成水陆游乐设施时间计算算法
    - 实现最早完成陆地和水上游乐设施时间的计算方法
    - 支持陆地先游玩及水上先游玩两种顺序的比较
    - 通过求最早完成时间返回最终结果
    - 使用双层遍历分别计算不同游乐设施组合完成时间
    - 设计代码结构清晰，便于理解和扩展

---
+++ algorithm/src/main/java/com/logos/leetcode/dayquestion2026/Jun/M06D02.java
@@ -0,0 +1,37 @@
+package com.logos.leetcode.dayquestion2026.Jun;
+
+/**
+ * @author logos
+ * date 2026/6/2 21:31
+ * @version v1.0
+ * @Package com.logos.leetcode.dayquestion2026.Jun
+ */
+public class M06D02 {
+
+    /**
+     * 3633. 最早完成陆地和水上游乐设施的时间 I
+     * <a href="https://leetcode.cn/problems/earliest-finish-time-for-land-and-water-rides-i/description/"/>
+     */
+
+    class Solution {
+        public int earliestFinishTime(int[] landStartTime, int[] landDuration, int[] waterStartTime, int[] waterDuration) {
+            int landWater = solve(landStartTime, landDuration, waterStartTime, waterDuration);
+            int waterLand = solve(waterStartTime, waterDuration, landStartTime, landDuration);
+            return Math.min(landWater, waterLand);
+        }
+
+        private int solve(int[] landStartTime, int[] landDuration, int[] waterStartTime, int[] waterDuration) {
+            int minFinish = Integer.MAX_VALUE;
+            for (int i = 0; i < landStartTime.length; i++) {
+                minFinish = Math.min(minFinish, landStartTime[i] + landDuration[i]);
+            }
+
+            int res = Integer.MAX_VALUE;
+            for (int i = 0; i < waterStartTime.length; i++) {
+                res = Math.min(res, Math.max(waterStartTime[i], minFinish) + waterDuration[i]);
+            }
+            return res;
+        }
+    }
+
+}
