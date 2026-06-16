a6f30d98dca7b3f39aec72d61159cdbb7afb2ac8
logos <2378884181@qq.com>
2026-06-16 22:25:56 +0800

    feat(algorithm): 添加字符串特殊操作处理算法
    - 新增 M06D16 类
    - 实现字符串处理功能，支持特殊字符 '*', '#', '%'
    - '*' 表示删除前一个字符
    - '#' 表示字符串自身拼接
    - '%' 表示字符串反转
    - 提供 processStr 方法返回处理后的字符串结果

---
+++ algorithm/src/main/java/com/logos/leetcode/dayquestion2026/Jun/M06D16.java
@@ -0,0 +1,36 @@
+package com.logos.leetcode.dayquestion2026.Jun;
+
+/**
+ * @author logos
+ * date 2026/6/16 22:25
+ * @version v1.0
+ * @Package com.logos.leetcode.dayquestion2026.Jun
+ */
+public class M06D16 {
+
+    /**
+     * 3612. 用特殊操作处理字符串 I
+     * <a href="https://leetcode.cn/problems/process-string-with-special-operations-i/description/"/>
+     */
+
+    class Solution {
+        String processStr(String s) {
+            StringBuilder ans = new StringBuilder();
+            for (char c : s.toCharArray()) {
+                if (c == '*') {
+                    if (!ans.isEmpty()) {
+                        ans.setLength(ans.length() - 1);
+                    }
+                } else if (c == '#') {
+                    ans.append(ans);
+                } else if (c == '%') {
+                    ans.reverse();
+                } else {
+                    ans.append(c);
+                }
+            }
+            return ans.toString();
+        }
+    }
+
+}
