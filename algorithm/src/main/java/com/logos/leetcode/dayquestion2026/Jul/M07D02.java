cb239eadf1ae0c329a7dbf28b5e7ffa5ce359c76
logos <2378884181@qq.com>
2026-07-02 20:36:13 +0800

    feat(grid): 实现穿越网格图的安全路径算法
    - 新增 M07D02 类及内部 Solution 类
    - 实现 findSafeWalk 方法，用于判断是否存在安全路径
    - 使用 0-1 BFS 算法优化路径搜索过程
    - 支持动态计算到达每个节点的最小代价
    - 根据健康值判断路径的可行性
    - 代码注释完善，便于理解与维护

---
+++ algorithm/src/main/java/com/logos/leetcode/dayquestion2026/Jul/M07D02.java
@@ -0,0 +1,60 @@
+package com.logos.leetcode.dayquestion2026.Jul;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.List;
+
+/**
+ * @author logos
+ * date 2026/7/2 20:35
+ * @version v1.0
+ * @Package com.logos.leetcode.dayquestion2026.Jul
+ */
+public class M07D02 {
+
+    /**
+     * 3286. 穿越网格图的安全路径
+     * <a href="https://leetcode.cn/problems/find-a-safe-walk-through-a-grid/description/"/>
+     */
+
+    class Solution {
+        private static final int[][] DIRS = {{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
+
+        public boolean findSafeWalk(List<List<Integer>> grid, int health) {
+            int m = grid.size();
+            int n = grid.get(0).size();
+            Integer[][] a = new Integer[m][];
+            int[][] dis = new int[m][n];
+            for (int i = 0; i < m; i++) {
+                a[i] = grid.get(i).toArray(Integer[]::new);
+                Arrays.fill(dis[i], Integer.MAX_VALUE);
+            }
+            dis[0][0] = a[0][0];
+            Deque<int[]> q = new ArrayDeque<>();
+            q.addFirst(new int[]{0, 0});
+            while (!q.isEmpty()) {
+                int[] p = q.pollFirst();
+                int i = p[0];
+                int j = p[1];
+                for (int[] d : DIRS) {
+                    int x = i + d[0];
+                    int y = j + d[1];
+                    if (0 <= x && x < m && 0 <= y && y < n) {
+                        int cost = a[x][y];
+                        if (dis[i][j] + cost < dis[x][y]) {
+                            dis[x][y] = dis[i][j] + cost;
+                            if (cost == 0) {
+                                q.addFirst(new int[]{x, y});
+                            } else {
+                                q.addLast(new int[]{x, y});
+                            }
+                        }
+                    }
+                }
+            }
+            return dis[m - 1][n - 1] < health;
+        }
+    }
+
+}
