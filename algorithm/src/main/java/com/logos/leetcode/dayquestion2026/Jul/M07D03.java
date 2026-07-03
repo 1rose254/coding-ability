034e90749932b2fab5fa9ffa837ac17d1c4304c5
logos <2378884181@qq.com>
2026-07-03 19:46:14 +0800

    feat(algorithm): 新增3620题网络路径恢复算法实现
    - 实现findMaxPathScore方法，通过二分搜索计算最大路径分数
    - 利用邻接表存储在线节点之间的边
    - 使用深度优先搜索和记忆化优化路径计算
    - 添加注释说明代码功能及题目链接

---
+++ algorithm/src/main/java/com/logos/leetcode/dayquestion2026/Jul/M07D03.java
@@ -0,0 +1,68 @@
+package com.logos.leetcode.dayquestion2026.Jul;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author logos
+ * date 2026/7/3 19:45
+ * @version v1.0
+ * @Package com.logos.leetcode.dayquestion2026.Jul
+ */
+public class M07D03 {
+
+    /**
+     * 3620. 恢复网络路径
+     * <a href="https://leetcode.cn/problems/network-recovery-pathways/description/"/>
+     */
+
+    class Solution {
+        public int findMaxPathScore(int[][] edges, boolean[] online, long k) {
+            int n = online.length;
+            List<int[]>[] g = new ArrayList[n];
+            Arrays.setAll(g, _ -> new ArrayList<>());
+            int maxWt = -1;
+            for (int[] e : edges) {
+                int x = e[0], y = e[1], wt = e[2];
+                if (online[x] && online[y]) {
+                    g[x].add(new int[]{y, wt});
+                    if (x == 0) {
+                        maxWt = Math.max(maxWt, wt);
+                    }
+                }
+            }
+
+            long[] memo = new long[n];
+            int left = -1, right = maxWt + 1;
+            while (left + 1 < right) {
+                int mid = left + (right - left) / 2;
+                Arrays.fill(memo, -1L);
+                if (dfs(0, mid, g, memo) <= k) {
+                    left = mid;
+                } else {
+                    right = mid;
+                }
+            }
+            return left;
+        }
+
+        private long dfs(int x, int lower, List<int[]>[] g, long[] memo) {
+            if (x == g.length - 1) {
+                return 0;
+            }
+            if (memo[x] != -1) {
+                return memo[x];
+            }
+            long res = Long.MAX_VALUE / 2;
+            for (int[] e : g[x]) {
+                int y = e[0], wt = e[1];
+                if (wt >= lower) {
+                    res = Math.min(res, dfs(y, lower, g, memo) + wt);
+                }
+            }
+            return memo[x] = res;
+        }
+    }
+
+}
