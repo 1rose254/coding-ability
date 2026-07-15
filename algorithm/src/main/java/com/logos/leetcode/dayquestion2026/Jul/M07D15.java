b61eb14d8646330553304a4b4c3444b1ba22d879
logos <2378884181@qq.com>
2026-07-15 22:35:24 +0800

    feat(algorithm): 添加2026年7月15日奇数偶数和最大公约数题目代码
    - 新增M07D15类及其Solution内部类
    - 实现gcdOfOddEvenSums方法，当前返回输入参数
    - 添加题目链接和注释说明
    - 完成基础框架搭建，待后续完善功能逻辑

---
+++ algorithm/src/main/java/com/logos/leetcode/dayquestion2026/Jul/M07D15.java
@@ -0,0 +1,22 @@
+package com.logos.leetcode.dayquestion2026.Jul;
+
+/**
+ * @author logos
+ * date 2026/7/15 22:34
+ * @version v1.0
+ * @Package com.logos.leetcode.dayquestion2026.Jul
+ */
+public class M07D15 {
+
+    /**
+     * 3658. 奇数和与偶数和的最大公约数
+     * <a href="https://leetcode.cn/problems/gcd-of-odd-and-even-sums/description/"/>
+     */
+
+    class Solution {
+        public int gcdOfOddEvenSums(int n) {
+            return n;
+        }
+    }
+
+}
