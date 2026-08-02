39e0a3c57c267ddfde66f8e4281c0b621aa33d93
logos <2378884181@qq.com>
2026-08-02 12:11:26 +0800

    feat(algorithm): 添加第2026年8月2日石子游戏题解模板
    - 新增M08D02类，包含877题石子游戏的初始解题框架
    - 提供石子游戏题目链接注释方便查阅题意
    - 实现stoneGame方法默认返回true，作为占位实现
    - 添加作者及版本信息注释完善代码规范

---
+++ algorithm/src/main/java/com/logos/leetcode/dayquestion2026/Aug/M08D02.java
@@ -0,0 +1,22 @@
+package com.logos.leetcode.dayquestion2026.Aug;
+
+/**
+ * @author logos
+ * date 2026/8/1 23:47
+ * @version v1.0
+ * @Package com.logos.leetcode.dayquestion2026.Aug
+ */
+public class M08D02 {
+
+    /**
+     * 877. 石子游戏
+     * <a href="https://leetcode.cn/problems/stone-game/description/"/>
+     */
+
+    class Solution {
+        public boolean stoneGame(int[] piles) {
+            return true;
+        }
+    }
+
+}
