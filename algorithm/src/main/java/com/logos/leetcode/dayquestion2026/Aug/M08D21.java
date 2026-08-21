07c0027667be9a90d320a3cf7cab5db3240d9d71
logos <2378884181@qq.com>
2026-08-21 09:55:17 +0800

    feat(leetcode): 添加3069题分配元素到两个数组的解决方案
    - 实现了将元素分配到两个数组的逻辑
    - 根据数组末尾元素大小决定新元素添加位置
    - 最终合并两个数组并返回结果
    - 包含题目链接作为注释说明
    - 创建了单个Solution类封装方法

---
+++ algorithm/src/main/java/com/logos/leetcode/dayquestion2026/Aug/M08D21.java
@@ -0,0 +1,41 @@
+package com.logos.leetcode.dayquestion2026.Aug;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author logos
+ * date 2026/8/21 9:54
+ * @version v1.0
+ * @Package com.logos.leetcode.dayquestion2026.Aug
+ */
+public class M08D21 {
+
+    /**
+     * 3069. 将元素分配到两个数组中 I
+     * <a href="https://leetcode.cn/problems/distribute-elements-into-two-arrays-i/description/"/>
+     */
+
+    class Solution {
+        public int[] resultArray(int[] nums) {
+            int n = nums.length;
+            List<Integer> a = new ArrayList<>();
+            List<Integer> b = new ArrayList<>();
+            a.add(nums[0]);
+            b.add(nums[1]);
+            for (int i = 2; i < n; i++) {
+                if (a.get(a.size() - 1) > b.get(b.size() - 1)) {
+                    a.add(nums[i]);
+                } else {
+                    b.add(nums[i]);
+                }
+            }
+            a.addAll(b);
+            for (int i = 0; i < n; i++) {
+                nums[i] = a.get(i);
+            }
+            return nums;
+        }
+    }
+
+}
