f639b7913d579b0cdd53eed98d6cf6eafae14d40
logos <2378884181@qq.com>
2026-08-26 10:13:56 +0800

    feat(algorithm): 实现最短且字典序最小的美丽子字符串算法
    - 添加M08D26类及其内部Solution类
    - 实现shortestBeautifulSubstring方法用于查找满足条件的最短子字符串
    - 根据条件过滤并更新当前答案字符串
    - 使用滑动窗口技巧优化子字符串搜索逻辑
    - 保证返回子字符串满足长度与字典序的最优性

---
+++ algorithm/src/main/java/com/logos/leetcode/dayquestion2026/Aug/M08D26.java
@@ -0,0 +1,40 @@
+package com.logos.leetcode.dayquestion2026.Aug;
+
+/**
+ * @author logos
+ * date 2026/8/26 10:13
+ * @version v1.0
+ * @Package com.logos.leetcode.dayquestion2026.Aug
+ */
+public class M08D26 {
+
+    /**
+     * 2904. 最短且字典序最小的美丽子字符串
+     * <a href="https://leetcode.cn/problems/shortest-and-lexicographically-smallest-beautiful-string/description/"/>
+     */
+
+    class Solution {
+        public String shortestBeautifulSubstring(String S, int k) {
+            if (S.replace("0", "").length() < k) {
+                return "";
+            }
+            char[] s = S.toCharArray();
+            String ans = S;
+            int cnt1 = 0, left = 0;
+            for (int right = 0; right < s.length; right++) {
+                cnt1 += s[right] - '0';
+                while (cnt1 > k || s[left] == '0') {
+                    cnt1 -= s[left++] - '0';
+                }
+                if (cnt1 == k) {
+                    String t = S.substring(left, right + 1);
+                    if (t.length() < ans.length() || t.length() == ans.length() && t.compareTo(ans) < 0) {
+                        ans = t;
+                    }
+                }
+            }
+            return ans;
+        }
+    }
+
+}
