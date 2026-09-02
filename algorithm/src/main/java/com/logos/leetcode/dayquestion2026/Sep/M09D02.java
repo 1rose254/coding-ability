34f6d7833c506a1d35db83cdb575510615e08f38
logos <2378884181@qq.com>
2026-09-02 11:30:20 +0800

    feat(algorithm): 添加构造奇偶一致数组题目初始代码
    - 创建了 M09D02 类文件，包含题目编号和名称注释
    - 编写了包含方法 uniformArray 的内部 Solution 类
    - uniformArray 方法暂时实现为固定返回 true
    - 添加了题目链接注释方便后续查阅题目详情

---
+++ algorithm/src/main/java/com/logos/leetcode/dayquestion2026/Sep/M09D02.java
@@ -0,0 +1,22 @@
+package com.logos.leetcode.dayquestion2026.Sep;
+
+/**
+ * @author logos
+ * date 2026/9/2 11:27
+ * @version v1.0
+ * @Package com.logos.leetcode.dayquestion2026.Sep
+ */
+public class M09D02 {
+
+    /**
+     * 3875. 构造奇偶一致的数组 I
+     * <a href="https://leetcode.cn/problems/construct-uniform-parity-array-i/description/"/>
+     */
+
+    class Solution {
+        public boolean uniformArray(int[] nums1) {
+            return true;
+        }
+    }
+
+}
